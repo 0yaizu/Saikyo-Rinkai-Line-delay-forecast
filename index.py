ff1f8be52e90099c1f947b8803b58dcf3c8dfc0a
OASIS <s2422008@stu.musashino-u.ac.jp>
2024-06-16 05:49:43 +0900

    database update

--- index.py
+++ index.py
@@ -1,10 +1,20 @@
 import sqlite3
 
+search_settings = {
+	"search_time": "first_seven",
+	"rain": True
+}
+
 def get_delay_percentage():
 	dbname = 'saikyo_line_delay_history.db'
 	conn = sqlite3.connect(dbname)
-	for row in conn.execute('SELECT COUNT(*) FROM first_seven WHERE delay_time > 0;'):
-		print(row[0])
+	delay_time = 0
+	all_count = 0
+	for row in conn.execute(f'SELECT COUNT(*) FROM first_seven WHERE {search_settings[search_time]} > 0;'):
+		delay_time = int(row[0])
+	for row in conn.execute(f'SELECT COUNT(*) FROM {search_settings[search_time]};'):
+		all_count = row[0]
+	print('遅延発生率: ' + str(delay_time / all_count * 100) + '%')
 	conn.close()
 
 get_delay_percentage()
