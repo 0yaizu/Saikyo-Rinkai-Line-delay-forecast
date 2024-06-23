92eae3d273b9bc60a536371ab86771e1123a2bc4
OASIS <s2422008@stu.musashino-u.ac.jp>
2024-06-23 10:51:00 +0900

    fix bug

--- get_latest_delay_info.py
+++ get_latest_delay_info.py
@@ -1,25 +1,32 @@
-import sqlite3, requests
+import sqlite3, requests, datetime
 from bs4 import BeautifulSoup
 
 req = requests.get('https://traininfo.jreast.co.jp/train_info/line.aspx?gid=1&lineid=saikyoline')
 req.encoding = req.apparent_encoding
+now = datetime.datetime.now()
 
 bsObj = BeautifulSoup(req.text, 'html.parser')
 
-items = bsObj.find_all('div', { 'class': 'basicTable02 tdCenter delayTable mb50'})[0].find_all('tr')[1]
+delay_latest = bsObj.find_all('div', { 'class': 'basicTable02 tdCenter delayTable mb50'})[0].find_all('tr')[1]
+delay_list = []
 
-# print('---')
-# for i in items.find_all('td'):
-# 	print(i)
-# 	print('---')
+if now.hour <= 5 and now.minute <= 0:
+	delay_list.append(datetime.datetime(now.year, now.month, now.day - 1).strftime('%Y-%m-%d'))
+else:
+	delay_list.append(now.date().strftime('%Y-%m-%d'))
 
-
-delay_befores = bsObj.find_all('div', { 'class': 'basicTable02 tdCenter delayTable mb50 accMore_cont'})[0].find_all('tr')[1]
-
-print('---')
-for i in delay_befores.find_all('td'):
+for i in delay_latest.find_all('td'):
 	if i.find('a') != None:
-		print(i.find('a').get_text().replace('分別ウィンドウで開きます', ''))
+		delay_list.append(int(i.find('a').get_text().replace('分別ウィンドウで開きます', '')))
 	else:
-		print(i.get_text())
-	print('---')
+		delay_list.append('""')
+print(delay_list)
+
+dbname = 'saikyo_line_delay_history.db'
+conn = sqlite3.connect(dbname)
+conn.execute(f"""CREATE TABLE IF NOT EXISTS delay_time (date Date, first_seven INT, seven_ten INT, ten_sixteen INT, sixteen_twentyone INT, twentytwo_last INT)""")
+
+print(f"""INSERT INTO delay_time VALUES ("{delay_list[0]} 00:00:00", {delay_list[1]}, {delay_list[2]}, {delay_list[3]}, {delay_list[4]}, {delay_list[5]});""")
+conn.execute(f"""INSERT INTO delay_time VALUES ("{delay_list[0]} 00:00:00", {delay_list[1]}, {delay_list[2]}, {delay_list[3]}, {delay_list[4]}, {delay_list[5]});""")
+
+conn.close()
--- index.py
+++ index.py
@@ -10,11 +10,13 @@ def get_delay_percentage():
 	conn = sqlite3.connect(dbname)
 	delay_time = 0
 	all_count = 0
-	for row in conn.execute(f'SELECT COUNT(*) FROM first_seven WHERE {search_settings[search_time]} > 0;'):
+	for row in conn.execute(f"""SELECT COUNT({search_settings["search_time"]}) FROM delay_time WHERE {search_settings["search_time"]};"""):
 		delay_time = int(row[0])
-	for row in conn.execute(f'SELECT COUNT(*) FROM {search_settings[search_time]};'):
-		all_count = row[0]
-	print('遅延発生率: ' + str(delay_time / all_count * 100) + '%')
+		print('遅延発生回数: ' + str(delay_time))
+	for row in conn.execute(f"""SELECT COUNT(*) FROM delay_time;"""):
+		all_count = int(row[0])
+		print('日数: ' + str(all_count))
+	print('遅延発生率: ' + str(round(delay_time / all_count * 100, 4)) + '%')
 	conn.close()
 
 get_delay_percentage()
